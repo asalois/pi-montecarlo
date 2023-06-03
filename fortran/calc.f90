4456098b0defa55974424a899acd72c652305feb
Alexander Salois <alexander.salois@montana.edu>
2023-06-03 15:04:41 -0600

    loops and prints

---
+++ fortran/calc.f90
@@ -0,0 +1,16 @@
+program calc
+  implicit none
+  integer :: i
+  real :: pi
+  real :: realpi
+  real :: darts
+  real :: hits
+  real :: trials
+  realpi = 3.1415927
+
+  do i = 1, 10
+    print *, 'i= ', i
+  end do
+  print *, 'Actual Pi= ', realpi
+
+end program calc
