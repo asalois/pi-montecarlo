9a87313da33207a5a00629ff30711814bb9f5f93
Alexander Salois <alexander.salois@montana.edu>
2024-03-08 16:58:16 -0700

    Add comments to explain a little more

--- fortran/calc.f90
+++ fortran/calc.f90
@@ -1,5 +1,6 @@
 program calc
   implicit none
+  ! declare variables
   integer :: i
   integer :: hits
   integer :: trials
@@ -10,19 +11,24 @@ program calc
   real :: realpi
   real :: darts
 
+  ! set some variables
   realpi = 3.1415927
   trials = 1000000
   hits = 0
 
+  ! throw one dart at the board per trial
   do i = 1, trials
     call random_number(x)
     call random_number(y)
     r = x**2 + y**2
+
+    ! if the dart hit the board then add one to hits
     if (r <= 1) then
       hits = hits + 1
     end if
   end do
 
+  ! calc pi by 4 times the ratio of hits to trials 
   mypi = 4 * real(hits) / real(trials)
   
   print *, 'Pi= ', mypi
