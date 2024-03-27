f40ee76c679e187d76115ce000ab00f489c626ed
Alex Salois <asalois.scholar@gmail.com>
2024-03-27 07:17:35 -0600

    Moved to a function

--- julia/calcPi.jl
+++ julia/calcPi.jl
@@ -2,26 +2,31 @@ using Printf
 using Random
 
 # inputs
-seed = parse(Int64, ARGS[1])
-throws = 100_000_000
-hits = 0
+inSeed = parse(Int64, ARGS[1])
+trials = 1_000_000_000
 
-rng = Xoshiro(seed)
-for i = 1:throws
-  x = rand(rng)
-  y = rand(rng)
-  r = x^2 + y^2
-  if r <= 1
-    global hits += 1
+
+function sim(throws, seed)
+  hits = 0
+  rng = Xoshiro(seed)
+
+  for i = 1:throws
+    x = rand(rng)
+    y = rand(rng)
+    r = x^2 + y^2
+    if r <= 1
+      hits += 1
+    end
   end
+  
+  myPi = 4 * hits / throws
+  error = 100 * (pi - myPi) / pi
+  
+  @printf("Throws = %d \n", throws)
+  @printf("myPi = %f \n", myPi)
+  @printf("Error = %f \n", error)
+  @printf("%f,%f,%d,%d\n", abs(error), myPi, throws, seed)
 end
 
-myPi = 4 * hits / throws
-error = 100 * (pi - myPi) / pi
-
-@printf("Throws = %d \n", throws)
-@printf("myPi = %f \n", myPi)
-@printf("Error = %f \n", error)
-@printf("%f,%f,%d,%d\n", abs(error), myPi, throws, seed)
-
-
+@time sim(trials, inSeed)
+sim(trials, inSeed)
