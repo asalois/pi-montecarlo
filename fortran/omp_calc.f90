9431440248ad9836e8ec6f0a5353aaf94a1717a8
Alex Salois <asalois.scholar@gmail.com>
2024-05-20 07:19:32 -0600

    Added an OpenMP example
    The Fortran examples seem to slower than Julia

---
+++ fortran/omp_calc.f90
@@ -0,0 +1,44 @@
+program calc
+  use omp_lib
+  implicit none
+  ! declare variables
+  integer :: i
+  integer :: hits
+  integer :: trials
+  real :: mypi
+  real :: r
+  real :: x
+  real :: y
+  real :: realpi
+  real :: darts
+
+  ! set some variables
+  realpi = 3.14159265358979323846264338327950288419939937
+  trials = 1000000000
+  hits = 0
+
+  ! set the number of threads
+  call omp_set_num_threads(8)
+
+  ! throw one dart at the board per trial
+  !$omp parallel do reduction(+:hits)
+  
+  ! throw one dart at the board per trial
+  do i = 1, trials
+    call random_number(x)
+    call random_number(y)
+    r = x**2 + y**2
+
+    ! if the dart hit the board then add one to hits
+    if (r <= 1) then
+      hits = hits + 1
+    end if
+  end do
+  !$omp end parallel do
+
+  ! calc pi by 4 times the ratio of hits to trials 
+  mypi = 4.0 * real(hits) / real(trials)
+  
+  print *, 'Pi= ', mypi
+
+end program calc
